e256660c2d42128f47b1ecd58465005d3b4b5897
Andrew Braun <andrew@andrewbraun.dev>
2021-05-21 19:27:32 +0400

    feat: Add selected state to buttons

--- components/mood-interactions/MoodGrid/MoodGrid.js
+++ components/mood-interactions/MoodGrid/MoodGrid.js
@@ -2,6 +2,19 @@ import MoodButton from "../MoodButton/MoodButton"
 import styles from "./mood-grid.module.css"
 
 export default function MoodGrid(props) {
-	const { moods } = props
-	return <div className={styles.moodGridContainer}></div>
+	const { moods, selectedMood, moodSelectHandler } = props
+
+	const moodButtons = moods.map((mood) => {
+		const { text, color } = mood
+		return (
+			<MoodButton
+				text={text}
+				color={color}
+				moodSelectHandler={moodSelectHandler}
+				selectedMood={selectedMood}
+				key={`${text}-${color}`}
+			/>
+		)
+	})
+	return <div className={styles.moodGridContainer}>{moodButtons}</div>
 }
--- pages/user/[...username]/index.js
+++ pages/user/[...username]/index.js
@@ -1,20 +1,40 @@
-import { Fragment } from "react"
-import MoodButton from "../../../components/mood-interactions/MoodButton/MoodButton"
-import styles from "./userprofile.module.css"
+import { Fragment, useState } from "react"
+import MoodGrid from "../../../components/mood-interactions/MoodGrid/MoodGrid"
+import styles from "./user-profile.module.css"
 
 export default function UserProfile() {
+	const [selectedMood, setSelectedMood] = useState("")
+
+	const moodSelectHandler = (event) => {
+		if (selectedMood === event.target.id) {
+			setSelectedMood("")
+			return
+		}
+		setSelectedMood(event.target.id)
+	}
+
 	const name = "Andrew"
+	const moods = [
+		{ text: "Bright", id: "bright", color: "yellow" },
+		{ text: "Cloudy", id: "cloudy", color: "blue" },
+	]
 
 	return (
 		<Fragment>
 			<h1>The many moods of {name}</h1>
+			<p>Today it's... {selectedMood}</p>
 			<main>
-				<div className={styles.setMoodContainer}>
-					<MoodButton text="Bright" id="bright" moodColor="yellow" />
+				<MoodGrid
+					moods={moods}
+					moodSelectHandler={moodSelectHandler}
+					selectedMood={selectedMood}
+				/>
+				{/* <div className={styles.setMoodContainer}>
+					<MoodButton text="Bright" id="bright" color="yellow" />
 				</div>
 				<div className={styles.setMoodContainer}>
-					<MoodButton text="Cloudy" id="cloudy" moodColor="blue" />
-				</div>
+					<MoodButton text="Cloudy" id="cloudy" color="blue" />
+				</div> */}
 			</main>
 		</Fragment>
 	)
